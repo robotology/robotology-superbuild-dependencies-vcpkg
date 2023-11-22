2ab94ad8dc15df3bc6f24ad42010b4695d5c0b6e
Silvio Traversaro <silvio@traversaro.it>
2023-11-22 17:13:39 +0100

    Delete gazebo-related files

--- scripts/setup-gazebo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-pushd %~dp0..
-set installerRootPath=%cd%
-popd
-
-
-set "PATH=%PATH%;%installerRootPath%\gazebo\install\bin"
-call %installerRootPath%\gazebo\install\share\gazebo\setup.bat
